66c183cb8c032276b81b0d7fd71756f01191f5c1
Daniel Sandberg <dsandber@andrew.cmu.edu>
2014-07-17 16:38:57 -0400

    Now registering all algorithms in Algorithms folder on module initialization

--- Src/CubeSolverModule/CubeSolverModule.cs
+++ Src/CubeSolverModule/CubeSolverModule.cs
@@ -7,6 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using RubiksApp.CubeSolverModule.View;
 using RubiksApp.CubeConfiguratorModule;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace RubiksApp.CubeSolverModule
 {
@@ -26,6 +29,28 @@ namespace RubiksApp.CubeSolverModule
         {
             ICubeRunnerRegistrar registrar = new CubeRunnerRegistrar(new CubeRunnerFactory(_cubeProvider));
             CubeRunnerPanelVM panelVm = new CubeRunnerPanelVM(registrar);
+            DirectoryInfo algorithmsDirectory = new DirectoryInfo("./Algorithms");
+            if(algorithmsDirectory.Exists)
+            {
+                foreach(FileInfo file in algorithmsDirectory.EnumerateFiles())
+                {
+                    Assembly dllToSearchForAlgorithms = null;
+
+                    try
+                    {
+                        dllToSearchForAlgorithms = Assembly.LoadFrom(file.FullName);
+                    }
+                    catch(Exception ex)
+                    {
+                        Trace.WriteLine(string.Format("Could not load file: {0}. Exception message: {1}.", file.FullName, ex.Message));
+                    }
+
+                    if(dllToSearchForAlgorithms != null)
+                    {
+                        registrar.Register(dllToSearchForAlgorithms);
+                    }
+                }
+            }
             _viewRegistry.RegisterViewWithRegion("solver", new Func<object>(() => new CubeRunnerPanel(){DataContext = panelVm}));
         }
     }
